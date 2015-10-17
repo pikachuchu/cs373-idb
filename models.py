d05913833d83dccaeaf9f335a94cbd92f8a6ddeb
Brian Chenault <brian.chenault@utexas.edu>
2015-10-17 16:55:52 -0500

    SQL model template, first upload

---
+++ models.py
@@ -0,0 +1,36 @@
+import os
+ 
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+ 
+ 
+Base = declarative_base()
+ 
+ 
+ 
+class Department(Base):
+    __tablename__ = 'department'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    employees = relationship(
+        'Employee',
+        secondary='department_employee_link'
+    )
+ 
+ 
+class Employee(Base):
+    __tablename__ = 'employee'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    hired_on = Column(DateTime, default=func.now())
+    departments = relationship(
+        Department,
+        secondary='department_employee_link'
+    )
+ 
+ 
+class DepartmentEmployeeLink(Base):
+    __tablename__ = 'department_employee_link'
+    department_id = Column(Integer, ForeignKey('department.id'), primary_key=True)
+    employee_id = Column(Integer, ForeignKey('employee.id'), primary_key=True)
