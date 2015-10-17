6106bbe122dd3ec403425253f9417fc1c6cf577e
Lawrence Ying <lying343@cs.utexas.edu>
2015-10-17 18:35:50 -0500

    made base template

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/senate')
+def senate():
+    return render_template('senate.html')
+
+@app.route('/house')
+def house():
+    return render_template('house.html')
+
 if __name__ == '__main__':
     app.run()
