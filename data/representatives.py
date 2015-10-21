367073a6df637ef145bcb6905558df32ea4ca817
Chuchu Ma <chuchu.ma@utexas.edu>
2015-10-20 21:29:15 -0500

    Use bioguide id to gather rep committee data.
    This closes #23

--- data/representatives.py
+++ data/representatives.py
@@ -41,24 +41,20 @@ for v in govtrack_result:
     image_req = requests.get(image_url)
     if image_req.status_code == 200:
         obj['image_jpg'] = image_url
-    else:
-        print(person['firstname'] + ' ' + person['lastname'])
+    #else:
+        #print(person['firstname'] + ' ' + person['lastname'])
 
     # sunlight committee stuff
-    sunlight_url = 'http://congress.api.sunlightfoundation.com/legislators?apikey=' + apikey + '&last_name=' + person['lastname']
-    bioguide_arr = requests.get(sunlight_url).json()['results']
-    if len(bioguide_arr) > 0:
-        bioguide_id = bioguide_arr[0]['bioguide_id']
-        committee_url = 'http://congress.api.sunlightfoundation.com/committees?apikey=' + apikey + '&member_ids=' + bioguide_id
-        committees = requests.get(committee_url).json()['results']
+    committee_url = 'http://congress.api.sunlightfoundation.com/committees?apikey=' + apikey + '&member_ids=' + person['bioguideid']
+    committees = requests.get(committee_url).json()['results']
 
-        committee_list = []
-        for v in committees:
-            committee_list.append({
-                'id': v['committee_id'], 
-                'name': v['name']
-            })
-        obj['committees'] = committee_list
+    committee_list = []
+    for v in committees:
+        committee_list.append({
+            'id': v['committee_id'], 
+            'name': v['name']
+        })
+    obj['committees'] = committee_list
 
     formatted_result[person['id']] = obj
 
@@ -66,5 +62,4 @@ with open('rep_data.json', 'w') as outfile:
     json.dump(formatted_result, outfile)
 
 print(formatted_result[400034])
-
 
