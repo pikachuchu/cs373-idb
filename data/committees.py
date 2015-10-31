0a84751f6d9366387c1c8a224e028af054ff47b2
Chuchu Ma <chuchu.ma@utexas.edu>
2015-10-31 14:50:53 -0500

    Update committee and rep data

--- data/committees.py
+++ data/committees.py
@@ -8,29 +8,34 @@ people_data = json.load(open('rep_data.json'))
 apikey = os.environ.get('SUNLIGHT_API_KEY')
 
 formatted = {}
+num = 0
 for v in committees:
     curr = v['committee']
     if not curr:
         curr = v
     obj = {
-        "id": curr['code'],
+        "id": num,
+        "committee_id": curr['code'],
         "name": curr['name'],
         "website": curr['url'],
         "chamber": curr['committee_type'],
         "jurisdiction": curr['jurisdiction']
     }
 
-    sunlight_url = 'http://congress.api.sunlightfoundation.com/committees?apikey=' + apikey + '&committee_id=' + curr['code'] + '&fields=members'
-    members = requests.get(sunlight_url).json()['results'][0]['members']
+    sunlight_url = 'http://congress.api.sunlightfoundation.com/committees?apikey=' + apikey + '&committee_id=' + curr['code'] + '&fields=members,subcommittee'
+    committee = requests.get(sunlight_url).json()['results'][0]
+    members = committee['members']
     chairperson = members[0]
     if chairperson['title'] == 'Chairman':
         chair = chairperson['legislator']
         obj['chair'] = {
             'name': chair['first_name'] + ' ' + chair['last_name'],
-            'id': chair['govtrack_id']
+            'id': chair['bioguide_id']
         }
-    formatted[curr['code']] = obj
+    obj['is_subcommittee'] = committee['subcommittee']
+    formatted[obj['id']] = obj
 
 with open('committee_data.json', 'w') as outfile:
     json.dump(formatted, outfile)
 
+print(formatted)
