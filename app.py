b4f73ea3e1d83e78c3a3c18d8dc2a496a624f161
Chuchu Ma <sarahma88@gmail.com>
2015-11-16 18:37:36 +0000

    SEO website

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify, make_response, abort
 from flask.ext.cors import CORS, cross_origin
 from api import api
+
 import subprocess
+import database as db
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -12,6 +14,17 @@ app.register_blueprint(api, url_prefix='/api/v1')
 @cross_origin()
 def index():
     return render_template('index.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    sitemap_xml = render_template('sitemap.xml')
+    response = make_response(sitemap_xml)
+    response.headers['Content-Type'] = 'application/xml'
+    return response
+
+@app.route('/robots.txt')
+def robots():
+    return render_template('robots.txt')
 
 @app.route('/senate')
 @cross_origin()
@@ -44,15 +57,41 @@ def about():
 
 @app.route('/legislator/id/<int:legislator_id>')
 def legislator(legislator_id):
-    return render_template('legislator.html', legislator_id = legislator_id)
+    try:
+        person = db.get_legislator_by_id(legislator_id, True)
+        person['gender'] = 'Male' if person['gender'] == 'Male' else 'Female'
+    except Exception as e:
+        print(e)
+    return render_template('legislator.html', person=person)
 
 @app.route('/committee/id/<int:committee_id>')
 def committee(committee_id):
-    return render_template('committee.html', committee_id = committee_id)
+    # TODO: handle invalid ids
+    try:
+        committee = db.get_committee_by_id(committee_id, True)
+        committee['chamber_url'] = committee['chamber']
+        committee['chamber'] = 'House' if committee['chamber'] == 'house' else 'Senate'
+        committee['juridicition'] = committee['jurisdiction'] if committee['jurisdiction'] else 'No Jurisdiction'
+        committee['is_subcommittee'] = 'Yes' if committee['is_subcommittee'] else 'No'
+    except Exception as e:
+        print(e)
+    return render_template('committee.html', committee=committee)
 
 @app.route('/bills/id/<int:bill_id>')
 def bill(bill_id):
-    return render_template('bill.html', bill_id = bill_id)
+    try:
+        bill = db.get_bill_by_id(bill_id, True)
+        if 'house_status' in bill:
+            bill['house_status'] = 'Pass' if bill['house_status'] == 'pass' else 'Fail'
+        else:
+            bill['house_status'] = 'N/A'
+        if 'senate_status' in bill:
+            bill['senate_status'] = 'Pass' if bill['senate_status'] == 'pass' else 'Fail'
+        else:
+            bill['senate_status'] = 'N/A'
+    except Exception as e:
+        print(e)
+    return render_template('bill.html', bill = bill)
 
 @app.route('/tests')
 def tests():
